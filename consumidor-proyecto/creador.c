676eaabbfd74ffdbe3b5292f25f84fe70153ed06
Dev-Researcher <174758270+dev-researcher@users.noreply.github.com>
2024-09-07 21:14:51 -0600

    400, 100

--- consumidor-proyecto/creador.c
+++ consumidor-proyecto/creador.c
@@ -133,7 +133,7 @@ void activate(GtkApplication *app, gpointer user_data) {
     // Crear ventana
     window = gtk_application_window_new(app);
     gtk_window_set_title(GTK_WINDOW(window), "Monitor del Sistema");
-    gtk_window_set_default_size(GTK_WINDOW(window), 400, 400);
+    gtk_window_set_default_size(GTK_WINDOW(window), 400, 100);
 
     // Crear un grid para colocar los widgets
     grid = gtk_grid_new();
